c181c9f2b1f373c4103ce06d3194ce65a7e8e839
deepa <deepakh2014@gmail.com>
2023-11-27 19:37:11 +0400

    modified:   api/controllers/auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -10,7 +10,7 @@ export const signup = async (req,res,next)=>{
     await newUser.save()
    res.status(201).json({message:"User created sucessfully"})
    } catch (error) {
-    next(erroeHandler(error));
+    next(error);
    }
    
 };
