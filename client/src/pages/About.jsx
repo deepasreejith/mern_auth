5b5e692643983bee3e26cc56b2242b81b5c4729d
deepa <deepakh2014@gmail.com>
2023-11-29 20:18:47 +0400

    cmplete home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      about
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+     <h1 className='text-3xl font-bold mb-4 text-slate-700'>About</h1>
+     <p>
+
+     </p>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      home
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+     <h1 className='text-3xl font-bold mb-4 text-slate-700'>Welcome to my Auth App</h1>
+     <p>
+
+     </p>
     </div>
   )
 }
